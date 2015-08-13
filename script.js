412bf6d1d6c5e4415df881b7cdcf4fd6ebafdf51
tiffanyxiao <txiao8415@bths.edu>
2015-08-13 09:16:34 -0400

    removed minuses
    debugging

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function addContact(){
-+	//find the place of the list
-+    var contactList = document.getElementById("contactList");  
-+    //get the name
-+    var name = document.getElementById('textinput1').value
-+    	//create a new list item	
-+        var newListItem = document.createElement("li");
-+		newListItem.setAttribute("data-theme","a");
-+		//create the ahref	
-+		var newLink = document.createElement("a");
-+		newLink.setAttribute("href","#history");
-+		//create the text	
-+        var newName = document.createTextNode(name);
-+		//put the name in the link
-+		newLink.appendChild(newName);
-+		//put the link in the list
-+        newListItem.appendChild(newLink);
-+		//put the list in the unordered list
-+        contactList.appendChild(newListItem);
-+}
+	//find the place of the list
+    var contactList = document.getElementById("contactList");  
+    //get the name
+    var name = document.getElementById('textinput1').value
+    	//create a new list item	
+        var newListItem = document.createElement("li");
+		newListItem.setAttribute("data-theme","a");
+		//create the ahref	
+		var newLink = document.createElement("a");
+		newLink.setAttribute("href","#history");
+		//create the text	
+        var newName = document.createTextNode(name);
+		//put the name in the link
+		newLink.appendChild(newName);
+		//put the link in the list
+        newListItem.appendChild(newLink);
+		//put the list in the unordered list
+        contactList.appendChild(newListItem);
+}
