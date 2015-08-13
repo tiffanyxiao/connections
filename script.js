9db8b3be920cce415d5ede97ca1c82b74c4aee1d
tiffanyxiao <txiao8415@bths.edu>
2015-08-13 11:04:28 -0400

    Update History

--- script.js
+++ script.js
@@ -16,3 +16,40 @@ function addContact() {
 		//put the list in the unordered list
         contactList.appendChild(newListItem);
 }
+
+function updateHistory(){
+var value = document.getElementById("options").value;
+	if (value == "call") {
+		//find where to put it
+		var history = document.getElementById("lines");
+		//create a new text item
+		var newText = document.createTextNode("Called");
+		//create a new p item 
+		var newLine = document.createElement("p");
+		//put the text in the p
+		newLine.appendChild(newText);
+		history.appendChild(newLine);
+	}		
+	if (value == "email") {
+		//find where to put it
+		var history = document.getElementById("lines");
+		//create a new text item
+		var newText = document.createTextNode("Emailed");
+		//create a new p item 
+		var newLine = document.createElement("p");
+		//put the text in the p
+		newLine.appendChild(newText);
+		history.appendChild(newLine);
+	}
+		if (value == "message") {
+		//find where to put it
+		var history = document.getElementById("lines");
+		//create a new text item
+		var newText = document.createTextNode("Messaged");
+		//create a new p item 
+		var newLine = document.createElement("p");
+		//put the text in the p
+		newLine.appendChild(newText);
+		history.appendChild(newLine);
+	}			
+}
